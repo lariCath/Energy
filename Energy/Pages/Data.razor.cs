c1f9fa263654db02914e711dd59e7b433fb50c94
Mepi21 <107041264+Mepi21@users.noreply.github.com>
2023-05-26 17:36:01 +0200

    Prevent arrows

--- Energy/Pages/Data.razor.cs
+++ Energy/Pages/Data.razor.cs
@@ -1,18 +1,38 @@
-﻿using Energy.Service;
+﻿using System.ComponentModel.Design;
+using Energy.Service;
 using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections;
+using System.Net.NetworkInformation;
+using System.ComponentModel.DataAnnotations;
+using Radzen.Blazor;
 
 namespace Energy.Pages;
 
 public partial class Data
 {
-    [Inject] public OverviewService OverviewService { get; set; }
+    [Inject] public OverviewService? OverviewService { get; set; }
 
-    private IEnumerable<EnergyData2> data;
+    private IEnumerable<EnergyData2>? data;
     private IEnumerable<WeatherData2>? weather;
+    Hashtable selection = new Hashtable();
+    Appliance? appliance;
 
+    int? value;
     protected override async Task OnInitializedAsync()
     {
         data = await OverviewService!.GetDataAsync();
         weather = await OverviewService!.GetWeather();
+    }
+    public enum Appliance
+    {
+        [Display(Description = "Household Appliance")]
+        householdAppliance,
+        [Display(Description = "Battery Storage")]
+        batteryStorage,
+        [Display(Description = "E-Car ")]
+        eCar,
+        [Display(Description = "Thermal Storage")]
+        thermalStorage
     }
 }
